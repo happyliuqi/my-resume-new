2a26b3036dc40e477719fb576a8c9243c2c0ef5c
happyliuqi <liuqi940719@126.com>
2019-01-25 09:44:51 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :xs="24" :sm="24" :md="5" :lg="6" :xl="5" class="part">
+        <div>1</div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="8" class="part">
+        <div>1</div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="7" :lg="6" :xl="11" class="part">
+        <div>1</div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
